adeb4751ffc9b73b83bb2788e85c0a849a4bdc10
Kristina <kristina.palmquist@gmail.com>
2025-07-01 11:26:08 +0200

    templates lab done

---
+++ django/templates_project/templates_project/templates_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'templates_app/index.html')
+
+def other(request):
+    return render(request, 'templates_app/other.html')
+
+def base(request):
+    return render(request, 'templates_app/base.html')
+
+def relative(request):
+    return render(request, 'templates_app/relative.html')
+

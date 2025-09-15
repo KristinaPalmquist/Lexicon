8f5bebbe77742300bfb1868535ad25a8c042e7a2
Kristina <kristina.palmquist@gmail.com>
2025-09-15 15:03:20 +0200

    lab02

---
+++ agents/lab02/chatbot/chatbot.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+import gradio as gr
+
+
+messages = [{
+    "role": "system",
+    "content": "You are a helpful and kind API assistant."
+},]
+
+
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        # chat = openai.ChatCompletion.create(
+        #     model="llama3.2",
+        #     messages=messages
+        # )
+        ollama = OpenAI(
+            base_url="http://localhost:11434/v1",
+            api_key='ollama'
+        )  # API key doesn't matter
+        model_name = "llama3.2"
+        chat = ollama.chat.completions.create(
+            model=model_name, 
+            messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+
+inputs = gr.Textbox(lines=7, label="Chat with AI")
+outputs = gr.Textbox(label="Reply")
+
+gr.Interface(
+    fn=chatbot,
+    inputs=inputs,
+    outputs=outputs,
+    title="AI chatbot",
+    description="Ask anything you want",
+    theme="compact"
+).launch(share=True)
---
+++ agents/lab02/lab02/ai_chat/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from dotenv import load_dotenv
+# import os
+from openai import OpenAI
+import Markdown
+
+load_dotenv()
+
+
+def home(request):
+    answer = None
+    question = None
+    if request.method == "POST":
+        question = request.POST.get("question")
+        answer = 'No question has been asked yet'
+        request.session['answer'] = answer
+        request.session['question'] = question
+        return redirect('home')
+    ollama = OpenAI(
+        base_url="http://localhost:11434/v1",
+        api_key='ollama'
+    )  # API key doesn't matter
+    model_name = "llama3.2"
+    response = ollama.chat.completions.create(model=model_name, messages=messages)
+    answer = response.choices[0].message.content
+
+    print(Markdown(answer))
+
+
+    # Retrieve and clear from session after redirect
+    if 'answer' in request.session:
+        answer = request.session.pop('answer')
+    if 'question' in request.session:
+        question = request.session.pop('question')
+
+    return render(request, "home.html", {
+        "answer": answer,
+        "question": question
+        })

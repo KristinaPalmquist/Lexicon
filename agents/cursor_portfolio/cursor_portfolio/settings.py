ab5767532373ae092fec16fec2b7b304e7ab4b90
Kristina <kristina.palmquist@gmail.com>
2025-09-16 15:34:24 +0200

    updating static_root and staticfiles_storage

--- agents/cursor_portfolio/cursor_portfolio/settings.py
+++ agents/cursor_portfolio/cursor_portfolio/settings.py
@@ -149,12 +149,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# if not DEBUG:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    STATICFILES_STORAGE = (
-        'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    )
+STATICFILES_STORAGE = (
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

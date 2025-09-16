5b2b3079f2afb983f6e28d64635b34a11cbf6340
Kristina <kristina.palmquist@gmail.com>
2025-09-16 15:29:41 +0200

    updated allowed hosts

--- agents/cursor_portfolio/cursor_portfolio/settings.py
+++ agents/cursor_portfolio/cursor_portfolio/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'lexicon-kzux.onrender.com',
+    '.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
